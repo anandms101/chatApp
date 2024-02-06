cb1051ade1b2ea06c366afde0e93285d08b384c4
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-02-06 18:27:04 +0530

    added event emitters

--- index.js
+++ index.js
@@ -16,8 +16,11 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
 
